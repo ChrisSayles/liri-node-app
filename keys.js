0d60731f895fd7f0f8b733563b9589b90d8f2c30
Chris Sayles <chrissayles@Chriss-MacBook-Pro-2.local>
2017-01-18 15:05:09 -0600

    updated liri project

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('keys.js loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'OvZEeEN5tpweHl7mJLno0BlwO',
+  consumer_secret: '41v9soTLlun3OXAmRR2RiHHIq9FhcXkkTgqqwRpNBCZo4Bk4yY',
+  access_token_key: '788548846657818625-Aufyb1rhSEisRmyLYD5aD45LFl2CBD1',
+  access_token_secret: 'pnmKMLY73RSD4FOB0fRZs10JPbYnRGBBpDPqY7eEVsycX',
+}
